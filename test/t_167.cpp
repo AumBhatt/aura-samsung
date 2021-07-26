b64be99ee9f0ea6770215bd2e086a92ca7a08a59
AumBhatt <aum9698@outlook.com>
2021-07-26 19:49:04 +0400

    Test files 2

--- test/t_167.cpp
+++ test/t_167.cpp
@@ -1,53 +1,44 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
 #include <cstdlib>
 #include <iostream>
 #include <string>
-#include <unistd.h>
 
-/* namespace beast = boost::beast;         // from <boost/beast.hpp>
-namespace http = beast::http;           // from <boost/beast/http.hpp>
-namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
-namespace net = boost::asio;            // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp> */
+using tcp = boost::asio::ip::tcp;               // from <boost/asio/ip/tcp.hpp>
+namespace websocket = boost::beast::websocket;  // from <boost/beast/websocket.hpp>
 
 // Sends a WebSocket message and prints the response
 int main(int argc, char** argv)
 {
     try
     {
-        std::string host = argv[1];
-        auto const  port = argv[2];
-        auto const  text = argv[3];
+        // Check command line arguments.
+        if(argc != 4)
+        {
+            std::cerr <<
+                "Usage: websocket-client-sync <host> <port> <text>\n" <<
+                "Example:\n" <<
+                "    websocket-client-sync echo.websocket.org 80 \"Hello, world!\"\n";
+            return EXIT_FAILURE;
+        }
+        auto const host = argv[1];
+        auto const port = argv[2];
+        auto const text = argv[3];
 
         // The io_context is required for all I/O
         boost::asio::io_context ioc;
 
         // These objects perform our I/O
-        boost::asio::ip::tcp::resolver resolver{ioc};
-        boost::beast::websocket::stream<boost::asio::ip::tcp::socket> ws{ioc};
+        tcp::resolver resolver{ioc};
+        websocket::stream<tcp::socket> ws{ioc};
 
         // Look up the domain name
         auto const results = resolver.resolve(host, port);
 
         // Make the connection on the IP address we get from a lookup
-        auto ep = boost::asio::connect(ws.next_layer(), results);
-
-        // Update the host_ string. This will provide the value of the
-        // Host HTTP header during the WebSocket handshake.
-        // See https://tools.ietf.org/html/rfc7230#section-5.4
-        host += ':' + std::to_string(ep.port());
-
-        // Set a decorator to change the User-Agent of the handshake
-/*         ws.set_option(boost::beast::websocket::stream_base::decorator(
-            [](boost::beast::websocket::request_type& req)
-            {
-                req.set(boost::beast::http::field::user_agent,
-                    std::string(BOOST_BEAST_VERSION_STRING) +
-                        " websocket-client-coro");
-            })); */
-        
-        // ws.next_layer().handshake(boost::asio::ssl::stream_base::client);
+        boost::asio::connect(ws.next_layer(), results.begin(), results.end());
 
         // Perform the websocket handshake
         ws.handshake(host, "/");
@@ -56,18 +47,18 @@ int main(int argc, char** argv)
         ws.write(boost::asio::buffer(std::string(text)));
 
         // This buffer will hold the incoming message
-        boost::beast::flat_buffer buffer;
+        boost::beast::multi_buffer buffer;
 
         // Read a message into our buffer
         ws.read(buffer);
 
         // Close the WebSocket connection
-        ws.close(boost::beast::websocket::close_code::normal);
+        ws.close(websocket::close_code::normal);
 
         // If we get here then the connection is closed gracefully
 
-        // The make_printable() function helps print a ConstBufferSequence
-        std::cout << boost::beast::make_printable(buffer.data()) << std::endl;
+        // The buffers() function helps print a ConstBufferSequence
+        std::cout << boost::beast::buffers_to_string(buffer.data()) << std::endl;
     }
     catch(std::exception const& e)
     {
